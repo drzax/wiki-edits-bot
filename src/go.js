774d3c3adbb818bc4a247865f49e0fe9772b3d10
Simon Elvery <simon@elvery.net>
2025-05-13 20:48:04 +1000

    Group adjacent contributions

--- src/go.js
+++ src/go.js
@@ -55,6 +55,28 @@ const go = async () => {
     });
   }
 
+  /** @type {{user: string; pageid: number; contributions: typeof newContributions}[]} */
+  const group = [];
+
+  // Group sequential contributions to the same page by the same contributor
+  const grouped = newContributions.reduce((grouped, record) => {
+    const last = grouped[grouped.length - 1];
+    if (
+      last &&
+      last.user === record.contribution.user &&
+      last.pageid === record.contribution.pageid
+    ) {
+      last.contributions.push(record);
+      return grouped;
+    }
+    grouped.push({
+      user: record.contribution.user,
+      pageid: record.contribution.pageid,
+      contributions: [record],
+    });
+    return grouped;
+  }, group);
+
   let masto =
     process.env.URL &&
     process.env.ACCESS_TOKEN &&
@@ -68,13 +90,23 @@ const go = async () => {
     flags: "a",
   });
 
-  newContributions.forEach(async (d) => {
-    const text = `Someone on a network controlled by ${d.prefix.description} (${
-      d.contribution.user
-    }) edited the ${d.contribution.title} Wikipedia page anonymously.\n\n${
-      d.contribution.comment ? d.contribution.comment + "\n\n" : ""
-    }\n\nhttps://en.wikipedia.org/w/index.php?diff=prev&oldid=${
-      d.contribution.revid
+  grouped.forEach(async (group) => {
+    const text = `Someone on a network controlled by ${
+      group.contributions[0].prefix.description
+    } (${group.contributions[0].contribution.user}) edited the ${
+      group.contributions[0].contribution.title
+    } Wikipedia page anonymously.\n\n${
+      group.contributions.length > 1
+        ? `${group.contributions.length} edits:\n`
+        : ""
+    }${group.contributions
+      .flatMap(({ contribution: { comment } }) =>
+        comment ? [`- ${comment}`] : []
+      )
+      .join("\n")}\n\nhttps://en.wikipedia.org/w/index.php?diff=${
+      group.contributions[0].contribution.revid
+    }&oldid=${
+      group.contributions[group.contributions.length - 1].contribution.parentid
     }`;
 
     if (process.argv.includes("--dry")) {
@@ -87,14 +119,15 @@ const go = async () => {
           visibility: "unlisted",
         }));
 
-      postsFile.write(JSON.stringify({ text, data: d, status }) + "\n");
+      postsFile.write(JSON.stringify({ text, data: group, status }) + "\n");
       if (status) console.log(`Posted ${status.url}`);
     }
   });
-
-  // Update the state file
-  state.lastRun = now;
-  writeFile(STATE_FILE, JSON.stringify(state, undefined, 2), "utf-8");
+  if (!process.argv.includes("--dry")) {
+    // Update the state file
+    state.lastRun = now;
+    writeFile(STATE_FILE, JSON.stringify(state, undefined, 2), "utf-8");
+  }
 };
 
 go();
